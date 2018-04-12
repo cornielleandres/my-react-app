af9016cec014e48655caf4be66e02bde04530b5c
Carlos Andres <35614736+cornielleandres@users.noreply.github.com>
2018-04-12 14:38:22 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
+import {Button} from "./Button";
+
 class App extends React.Component {
+  const text = "Click this";
   render() {
-    return <h1>My React App!!</h1>;
+    return (
+      <h1>!My React App!</h1>
+      <Button />
+    );
   }
 };
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App buttontext={text} />, document.getElementById('app'));
