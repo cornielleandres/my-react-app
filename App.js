d0cfda364899f7d7ff7caa737cf30744db3dbba3
Carlos Andres <35614736+cornielleandres@users.noreply.github.com>
2018-04-12 15:06:29 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import {Button} from './Button.js';
 class App extends React.Component {
   render() {
     return (
-      <h1>Three</h1>
+      <h1>Button there</h1>
+      <Button />
     );
   }
 };
