e480e619f7e71313f23f67be0b35b4bcfc615a47
Carlos Andres <35614736+cornielleandres@users.noreply.github.com>
2018-04-12 14:30:12 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 class App extends React.Component {
-        render() {
-          return <h1>My App in React!</h1>;
-        }
-      };
-      
-      ReactDOM.render(<App />, document.getElementById('app'));
+  render() {
+    return <h1>My React App!!</h1>;
+  }
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
